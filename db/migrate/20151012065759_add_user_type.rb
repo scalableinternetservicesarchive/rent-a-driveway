f1d38e72e69bc058b6ee6640d0d3aa595f1a0612
Yuuma Sakakibara <yuuma.sakakibara94@gmail.com>
2015-10-12 22:48:34 -0700

    removed redundant migrations

--- db/migrate/20151012065759_add_user_type.rb
+++
@@ -1,7 +0,0 @@
-class AddUserType < ActiveRecord::Migration
-  def change
-  	add_column :users, :is_admin, :boolean, default: false
-  	add_column :users, :is_buyer, :boolean, default: false
-  	add_column :users, :is_seller, :boolean, default: false
-  end
-end
