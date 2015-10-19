7ed70e4ad2fc0002ed9bd8e87c78326ad10da545
Yuuma Sakakibara <yuuma.sakakibara94@gmail.com>
2015-10-19 13:10:27 -0700

    Added transaction example to seed.rb .

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,9 @@
 class Listing < ActiveRecord::Base
-	validates :start_time, presence: true
+  validates :start_time, presence: true
   validates :end_time, presence: true
-	validates :price, presence: true
+  validates :price, presence: true
   validates :address, presence: true
-	belongs_to :owner, :class_name => 'User', :foreign_key => 'owner_id'
+  belongs_to :owner, :class_name => 'User', :foreign_key => 'owner_id'
   geocoded_by :address
   after_validation :geocode
   enum status: [:OPEN, :CLOSED]
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(first_name: 'Buyer', last_name: '1',
-            is_buyer: true, email: 'buyer_1@gmail.com', 
-            password: 'password', password_confirmation: 'password')
+buyer_1 = User.create(first_name: 'Buyer', last_name: '1',
+                      is_buyer: true, email: 'buyer_1@gmail.com', 
+                      password: 'password', password_confirmation: 'password')
 
 User.create(first_name: 'Buyer', last_name: '2',
             is_buyer: true, email: 'buyer_2@gmail.com', 
@@ -22,11 +22,12 @@ seller_2 = User.create(first_name: 'Seller', last_name: '2',
                      is_seller: true, email: 'seller_2@gmail.com',
                      password: 'password', password_confirmation: 'password')
 
-Listing.create( owner_id: seller_1.id,
-                address: '90024', 
-                start_time: DateTime.strptime("01/01/2015 17:00", "%m/%d/%Y %H:%M"),
-                end_time: DateTime.strptime("01/02/2015 17:00", "%m/%d/%Y %H:%M"),
-                price: 200)
+listing_1 = Listing.create( owner_id: seller_1.id,
+                            address: '90024', 
+                            start_time: DateTime.strptime("01/01/2015 17:00", "%m/%d/%Y %H:%M"),
+                            end_time: DateTime.strptime("01/02/2015 17:00", "%m/%d/%Y %H:%M"),
+                            price: 200,
+                            status: 'CLOSED')
 
 Listing.create( owner_id: seller_1.id,
                 address: '90025',
@@ -45,3 +46,10 @@ Listing.create( owner_id: seller_2.id,
                 start_time: DateTime.strptime("01/03/2015 17:00", "%m/%d/%Y %H:%M"),
                 end_time: DateTime.strptime("01/04/2015 17:00", "%m/%d/%Y %H:%M"),
                 price: 200)
+
+Transaction.create(seller_id: seller_1.id,
+                   buyer_id: buyer_1.id,
+                   listing_id: listing_1.id)
+
+# This seed creates 2 sellers and 2 buyer accounts.  It also creates 4 listings, 2 for each seller. 
+# One transaction is done between buyer_1 and seller_1 on listing_1.
