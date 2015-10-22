8c77079164eb30acbb1c5d38f81d32bbd851435d
Yuuma Sakakibara <yuuma.sakakibara94@gmail.com>
2015-10-22 01:01:58 -0700

    Fixed seed.rb typo Added address to listings views, fixed transactions page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-50.times { Fabricate(:listing_analtic) }
+50.times { Fabricate(:listing_analytic) }
 50.times { Fabricate(:user, is_buyer: true) }
 1.times { Fabricate(:user, is_admin: true, email: "admin@rad.com") }
