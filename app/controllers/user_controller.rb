075beac1d5eb80db128a99128ed984ef84e81971
John Lee <johnlee07@ucla.edu>
2015-10-22 17:51:44 -0700

    had to redecorate pages; added more info to profile page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,5 +6,21 @@ class UserController < ApplicationController
   def show
     @user = User.find(params[:id])
     @listings = @user.listings
+	@type = ""
+	if @user.is_admin
+		@type += "Admin"
+	end
+	if @user.is_buyer
+		if not @type.empty?
+			@type += ", "
+		end
+		@type += "Buyer"
+	end
+	if @user.is_seller
+		if not @type.empty?
+			@type += ", "
+		end
+		@type += "Seller"
+	end
   end
 end
