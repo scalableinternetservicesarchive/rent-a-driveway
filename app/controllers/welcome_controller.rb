22a73b14c72389ba01a89f846a859f51f2729224
mqchau <quanqchau@gmail.com>
2015-10-27 22:10:11 -0700

    search button on welcome page now actually does search and populate the map

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,29 @@
+require 'geokit'
+
 class WelcomeController < ApplicationController
     def index
-        @cur_lat = Rails.env.production? ? request.location.latitude : 34.068921
-        @cur_lng = Rails.env.production? ? request.location.longitude : -118.445181
-        @nearby_listings = Listing.near([@cur_lat, @cur_lng], 2)
+        if params.has_key?(:location) and params.has_key?(:latitude) then
+            a=Geokit::Geocoders::GoogleGeocoder.geocode params[:location]
+            @cur_lat = a.lat
+            @cur_lng = a.lng
+            @got_startdatetime = true
+            @start_datetime = "#{params[:start_date]} #{params[:start_time]}"
+            @end_datetime = "#{params[:end_date]} #{params[:end_time]}"
+        elsif Rails.env.production? then
+            @cur_lat = request.location.latitude
+            @cur_lng = request.location.longitude
+            @got_startdatetime = false 
+        else
+            @cur_lat = 34.068921
+            @cur_lng = -118.445181
+            @got_startdatetime = false 
+        end
+        
+        if @got_startdatetime then
+            @nearby_listings = Listing.near([@cur_lat, @cur_lng], 2).where("start_time <= :start_time AND end_time >= :end_time", {start_time: @start_datetime,end_time: @end_datetime})
+        else
+            @nearby_listings = Listing.near([@cur_lat, @cur_lng], 2)
+        end
         @markers = Gmaps4rails.build_markers(@nearby_listings) do |listing, marker|
             marker.lat listing.latitude
             marker.lng listing.longitude
