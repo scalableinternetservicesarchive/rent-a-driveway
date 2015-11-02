dd669b0dd0f8ec79edf9750e009a2c45ee902dec
John Lee <johnlee07@ucla.edu>
2015-11-02 00:51:18 -0800

    nearby listings listed when searching

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,13 +13,13 @@ class WelcomeController < ApplicationController
         elsif Rails.env.production? then
             @cur_lat = request.location.latitude
             @cur_lng = request.location.longitude
-            @got_startdatetime = false 
+            @got_startdatetime = false
         else
             @cur_lat = 34.068921
             @cur_lng = -118.445181
             @got_startdatetime = false 
         end
-        
+
         if @got_startdatetime then
             @nearby_listings = Listing.near([@cur_lat, @cur_lng], 2).where("start_time <= :start_time AND end_time >= :end_time", {start_time: @start_datetime,end_time: @end_datetime})
         else
@@ -30,8 +30,7 @@ class WelcomeController < ApplicationController
             marker.lng listing.longitude
             marker.infowindow  "Address: #{listing.address}<br />Price: $#{listing.price}<br /><a href=#{listing_path(listing)}>Details</a>"
         end
-	#TODO: list based on search
-	  @listing = Listing.all.order(sort_column + " " + sort_direction)
+      #TODO: search by price and make start time and end time optional
     end
   private
     def sort_column
